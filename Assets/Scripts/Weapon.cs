433edef1f48f58b3d963b8c2118d98b656ebdbac
Olli Havilehto <olli@havilehto.fi>
2020-12-25 16:26:31 +0200

    Add particle effect & lightning for bullet hit

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,6 +6,8 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 20;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
 
     // Update is called once per frame
     void Update()
@@ -18,10 +20,21 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRayCast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRayCast()
+    {
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("Player shoots and hits: " + hit.transform.name);
+            CreateHitImpact(hit);
             // Todo: Add hit effect visual for players
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target == null) return;
@@ -31,7 +44,11 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
-
     }
 
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, .1f);
+    }
 }
