3c804d435b8c0879f3ef06e19cfe952e3b326496
Olli Havilehto <olli@havilehto.fi>
2020-12-25 14:34:33 +0200

    Add enemy health & weapon damage system, update enemy model and materials

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    [SerializeField] float hitPoints = 100f;
+
+    public void TakeDamage(float damage)
+    {
+
+        hitPoints -= damage;
+        if (hitPoints <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
@@ -7,11 +5,12 @@ public class Weapon : MonoBehaviour
 
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 20;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
@@ -20,8 +19,19 @@ public class Weapon : MonoBehaviour
     private void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        Debug.Log("Player shoots and hits: " + hit.transform.name);
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("Player shoots and hits: " + hit.transform.name);
+            // Todo: Add hit effect visual for players
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+
     }
 
 }
